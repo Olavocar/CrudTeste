cb2d5460cfb3cb88e2286803c0316136bb7da31a
[vola_o] <113613682+Olavocar@users.noreply.github.com>
2022-12-22 10:43:53 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+include('config.php')
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <form action="" method="POST">
+    <p>     
+        <label>Email:</label>
+            <input type="text" name="email">
+    </p>
+    <p>     
+        <label>senha:</label>
+            <input type="password" name="senha">
+    </p>
+    <p>
+            <button type="submit">Entrar</button>
+    </p>
+    </form>
+</body>
+</html>
