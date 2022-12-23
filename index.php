bf9ed2e5f2035e43dff6d1d0be561c181977a9b6
[vola_o] <113613682+Olavocar@users.noreply.github.com>
2022-12-23 08:08:13 -0300

    13o commit

--- index.php
+++ index.php
@@ -22,9 +22,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" rel="stylesheet">
     <title>Login</title>
 </head>
 <body>
+    <div class="box">
     <form action="" method="POST">
     <p>     
         <label>Email:</label>
@@ -38,5 +40,6 @@
             <button type="submit">Entrar</button>
     </p>
     </form>
+    </div>
 </body>
 </html>
